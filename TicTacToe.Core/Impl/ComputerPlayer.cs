e5005e583dcec931466eac49d5722ce1bdfad84f
Kalantyr <kalantyr@yandex.ru>
2021-04-24 12:57:02 +0300

    Третий коммит

--- TicTacToe.Core/Impl/ComputerPlayer.cs
+++ TicTacToe.Core/Impl/ComputerPlayer.cs
@@ -4,16 +4,24 @@ namespace TicTacToe.Core.Impl
 {
     public class ComputerPlayer: IPlayer
     {
+        private readonly Random _rand = new Random();
+
         public (byte, byte) NextMove(IGameInfo game)
         {
             if (game == null) throw new ArgumentNullException(nameof(game));
 
             // TODO: тут нужно решить, куда компьютеру поставить "нолик"
 
-            byte x = 0;
-            byte y = 0;
+            // этот алгоритм выдаёт случайную пустую клетку
+            do
+            {
+                var x = (byte)_rand.Next(0, 3);
+                var y = (byte)_rand.Next(0, 3);
+                if (game.CurrentState[x, y] == null)
+                    return (x, y);
+            } while (true);
 
-            return (x, y);
+            //return (x, y);
         }
     }
 }
