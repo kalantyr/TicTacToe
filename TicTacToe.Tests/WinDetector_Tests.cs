b10528df0b9e312cbb417d4b462d8fd1264522c6
YuraKProgrammer <yura.kalantyr@mail.ru>
2021-04-24 14:01:57 +0300

    Реализован поиск победителя по горизонтали

--- TicTacToe.Tests/WinDetector_Tests.cs
+++ TicTacToe.Tests/WinDetector_Tests.cs
@@ -20,6 +20,40 @@ namespace TicTacToe.Tests
             var winDetector = new WinDetector();
             var result = winDetector.GetWinner(game.Object);
             Assert.IsNull(result);
+        }       
+       
+        [Test]
+        public void GetWinner_Test_2()
+        {
+            var state = new State?[3, 3];
+            state[0, 0] = State.Cross;
+            state[1, 0] = State.Cross;
+            state[2, 0] = State.Cross;
+
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var winDetector = new WinDetector();
+            var result = winDetector.GetWinner(game.Object);
+            Assert.AreEqual(Player.Human, result);
+        }
+       
+        [Test]
+        public void GetWinner_Test_3()
+        {
+            var state = new State?[3, 3];
+            state[0, 0] = State.Cross;
+            state[1, 0] = State.Zero;
+            state[2, 0] = State.Cross;
+
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var winDetector = new WinDetector();
+            var result = winDetector.GetWinner(game.Object);
+            Assert.AreEqual(null, result);
         }
     }
 }
