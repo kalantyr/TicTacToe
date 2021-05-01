52fd0c2d3b75af16429d87627646746a32ba2650
YuraKProgrammer <yura.kalantyr@mail.ru>
2021-05-01 13:17:09 +0300

    Доделан WinDetector

--- TicTacToe.Core/Impl/WinDetector.cs
+++ TicTacToe.Core/Impl/WinDetector.cs
@@ -18,6 +18,14 @@ namespace TicTacToe.Core.Impl
             
             result = GetVerticalWinner(game);
             if (result != null)
+                return result;           
+            
+            result = GetDiagonalWinner1(game);
+            if (result != null)
+                return result;
+
+            result = GetDiagonalWinner2(game);
+            if (result != null)
                 return result;
             
             return null;
@@ -46,7 +54,68 @@ namespace TicTacToe.Core.Impl
 
         private Player? GetVerticalWinner(IGameInfo game)
         {
+            for (var x = 0; x<3; x++)
+            {
+                var crossCount = 0;
+                var zeroCount = 0;
+                for (var y = 0; y<3; y++)
+                {
+                    if (game.CurrentState[x, y] == State.Cross)
+                        crossCount++;
+                    if (game.CurrentState[x, y] == State.Zero)
+                        zeroCount++;
+                }
+                if (crossCount == 3)
+                    return Player.Human;                
+                if (zeroCount == 3)
+                    return Player.Computer;
+            }
             return null;
         }
+        
+        private Player? GetDiagonalWinner1(IGameInfo game)
+        {
+                var crossCount = 0;
+                var zeroCount = 0;
+            for (var x = 0; x<3; x++)
+            {
+                var y = x;
+                if (game.CurrentState[x, y] == State.Cross)
+                    crossCount++;
+
+                if (game.CurrentState[x, y] == State.Zero)
+                    zeroCount++;
+
+                if (crossCount == 3)
+                    return Player.Human;
+                if (zeroCount == 3)
+                    return Player.Computer;
+            }
+            return null;
+        }        
+        
+        private Player? GetDiagonalWinner2(IGameInfo game)
+        {
+                var crossCount = 0;
+                var zeroCount = 0;
+            for (var x = 2; x > -1; x--)
+            {
+                var y = 2 - x;
+                if (game.CurrentState[x, y] == State.Cross)
+                {
+                    crossCount++;
+                }
+                if (game.CurrentState[x, y] == State.Zero)
+                    zeroCount++;
+
+                if (crossCount == 3)
+                    return Player.Human;
+                if (zeroCount == 3)
+                    return Player.Computer;
+            }
+            return null;        
+        }
+            
+       
     }
 }
