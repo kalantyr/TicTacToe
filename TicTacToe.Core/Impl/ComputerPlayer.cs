3cd593c3dc5415f3ab6abbd099d4d26d9e949148
YuraKProgrammer <yura.kalantyr@mail.ru>
2021-05-05 17:00:44 +0300

    Добавлен неполный алгоритм выбора места для 3 нолика

--- TicTacToe.Core/Impl/ComputerPlayer.cs
+++ TicTacToe.Core/Impl/ComputerPlayer.cs
@@ -15,6 +15,18 @@ namespace TicTacToe.Core.Impl
             var xy = GetHorizontalLastMove(game);
             if (xy != null)
                 return xy.Value;
+            
+            xy = GetVerticalLastMove(game);
+            if (xy != null)
+                return xy.Value;
+            
+            xy = GetDiagonal1LastMove(game);
+            if (xy != null)
+                return xy.Value;  
+            
+            xy = GetDiagonal2LastMove(game);
+            if (xy != null)
+                return xy.Value;
 
             // этот алгоритм выдаёт случайную пустую клетку
             do
@@ -47,6 +59,76 @@ namespace TicTacToe.Core.Impl
                 if (zeroCount == game.Size - 1)
                     if (nullCount > 0)
                         return (nullX, y);
+            }
+            return null;
+        }        
+        
+        public (byte, byte)? GetVerticalLastMove(IGameInfo game)
+        {
+            for (byte x = 0; x < game.Size; x++)
+            {
+                var zeroCount = 0;
+                var nullCount = 0;
+                byte nullY = 0;
+                for (byte y = 0; y < game.Size; y++)
+                {
+                    if (game.CurrentState[x, y] == null)
+                    {
+                        nullCount++;
+                        nullY = y;
+                    }
+                    if (game.CurrentState[x, y] == State.Zero)
+                        zeroCount++;
+                }
+                if (zeroCount == game.Size - 1)
+                    if (nullCount > 0)
+                        return (x, nullY);
+            }
+            return null;
+        }
+        
+        public (byte, byte)? GetDiagonal1LastMove(IGameInfo game)
+        {
+            for (byte x = 0; x < game.Size; x++)
+            {
+                var zeroCount = 0;
+                var nullCount = 0;
+                byte nullY = 0;
+                byte y = x;
+                if (game.CurrentState[x, y] == null)
+                {
+                    nullCount++;
+                    nullY = y;
+                }
+                if (game.CurrentState[x, y] == State.Zero)
+                    zeroCount++;
+                if (zeroCount == game.Size - 1)
+                    if (nullCount > 0)
+                        return (nullY, nullY);
+            }
+            return null;
+        }   
+        
+        public (byte, byte)? GetDiagonal2LastMove(IGameInfo game)
+        {
+            for (byte x = 0; x < game.Size; x++)
+            {
+                var zeroCount = 0;
+                var nullCount = 0;
+                byte nullX = 0;
+                byte nullY = 0;
+                byte y = (byte)(2-x);
+                if (game.CurrentState[x, y] == null)
+                {
+                    nullCount++;
+                    nullX = x;
+                    nullY = y;
+                }
+                    if (game.CurrentState[x, y] == State.Zero)
+                        zeroCount++;
+                if (zeroCount == game.Size - 1)
+                    if (nullCount > 0)
+                        return (nullX, nullY);
             }
             return null;
         }
--- TicTacToe.Tests/ComputerPlayer_Tests.cs
+++ TicTacToe.Tests/ComputerPlayer_Tests.cs
@@ -24,5 +24,112 @@ namespace TicTacToe.Tests
             Assert.AreEqual(1, x);
             Assert.AreEqual(0, y);
         }
+        
+        [Test]
+        public void GetVerticalLastMove_Test()
+        {
+            var state = new State?[3, 3];
+            state[0, 0] = State.Zero;
+            state[0, 1] = State.Zero;
+            state[0, 2] = null;
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.Size).Returns(3);
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var computerPlayer = new ComputerPlayer();
+            var (x, y) = computerPlayer.NextMove(game.Object);
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(2, y);
+        }  
+        
+        [Test]
+        public void GetVerticalLastMove_Test2()
+        {
+            var state = new State?[3, 3];
+            state[1, 0] = null;
+            state[1, 1] = State.Zero;
+            state[1, 2] = State.Zero;
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.Size).Returns(3);
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var computerPlayer = new ComputerPlayer();
+            var (x, y) = computerPlayer.NextMove(game.Object);
+            Assert.AreEqual(1, x);
+            Assert.AreEqual(0, y);
+        }     
+        
+        [Test]
+        public void GetDiagonal1LastMove_Test()
+        {
+            var state = new State?[3, 3];
+            state[0, 0] = State.Zero;
+            state[1, 1] = null;
+            state[2, 2] = State.Zero;
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.Size).Returns(3);
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var computerPlayer = new ComputerPlayer();
+            var (x, y) = computerPlayer.NextMove(game.Object);
+            Assert.AreEqual(1, x);
+            Assert.AreEqual(1, y);
+        } 
+        [Test]
+        public void GetDiagonal1LastMove_Test2()
+        {
+            var state = new State?[3, 3];
+            state[0, 0] = null;
+            state[1, 1] = State.Zero;
+            state[2, 2] = State.Zero;
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.Size).Returns(3);
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var computerPlayer = new ComputerPlayer();
+            var (x, y) = computerPlayer.NextMove(game.Object);
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(0, y);
+        }     
+        
+        [Test]
+        public void GetDiagonal2LastMove_Test()
+        {
+            var state = new State?[3, 3];
+            state[0, 2] = State.Zero;
+            state[1, 1] = State.Zero;
+            state[2, 0] = null;
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.Size).Returns(3);
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var computerPlayer = new ComputerPlayer();
+            var (x, y) = computerPlayer.NextMove(game.Object);
+            Assert.AreEqual(2, x);
+            Assert.AreEqual(0, y);
+        }      
+        
+        [Test]
+        public void GetDiagonal2LastMove_Test2()
+        {
+            var state = new State?[3, 3];
+            state[0, 2] = null;
+            state[1, 1] = State.Zero;
+            state[2, 0] = State.Zero;
+
+            var game = new Mock<IGameInfo>();
+            game.Setup(g => g.Size).Returns(3);
+            game.Setup(g => g.CurrentState).Returns(state);
+
+            var computerPlayer = new ComputerPlayer();
+            var (x, y) = computerPlayer.NextMove(game.Object);
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(2, y);
+        }
     }
 }
